86032a8eae6b99efd706dc5bd47b453d195d4615
César Escobedo <cesarescobedo@MacBook-Pro-de-Cesar-2.local>
2022-09-22 09:22:01 -0700

    primer-commit

--- navegacion/4.swift
+++ navegacion/4.swift
@@ -1,23 +1,21 @@
 //
-//  ViewController.swift
+//  4.swift
 //  navegacion
 //
 //  Created by Alumno on 15/09/22.
 //  Copyright © 2022 César Escobedo. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class cuatroController : UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-    @IBAction func doTapAtras(_ sender: Any) {
-        
-        self.dismiss(animated: true, completion: nil)
-    }
+    
+    
     
 }
-
--- navegacion/6.swift
+++ navegacion/6.swift
@@ -1,23 +1,18 @@
 //
-//  ViewController.swift
+//  6.swift
 //  navegacion
 //
 //  Created by Alumno on 15/09/22.
 //  Copyright © 2022 César Escobedo. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
-class ViewController: UIViewController {
-
+class Controller : UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
-    @IBAction func doTapAtras(_ sender: Any) {
-        
-        self.dismiss(animated: true, completion: nil)
-    }
-    
 }
-
